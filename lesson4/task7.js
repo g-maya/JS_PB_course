42dc30a1350eac4ba6b6819abf17c3a576bb1c6e
g-maya <gololobova92@gmail.com>
2022-01-30 21:38:34 +0200

    task7

---
+++ lesson4/task7.js
@@ -0,0 +1,19 @@
+/*
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+
+var arrLength = [];
+for (var i = 0; i < arr.length; i++) {
+arrLength[i] = arr[i].length;
+}
+
+console.log( arrLength ); // 4,5,2,3
+*/
+
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+
+
+var arrLength = arr.map(function(arr) {
+    return arr.length;
+  });
+  
+  console.log(arrLength); // 7,4,7,8
